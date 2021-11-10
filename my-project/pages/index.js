7ddbe62d50899508f43843ee1f8f8581fa9ee2af
jcalbrecht <jcalbrecht85@gmail.com>
2021-11-10 16:12:08 -0600

    font weight

--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -18,12 +18,12 @@ export default function Home() {
           Hi, I’m Jacob
         </h1>
 
-        <h2 className="text-gray-600 dark:text-gray-400 mb-4 mt-4 font-light tracking-wide leading-loose">
-          I’m a Backend Web 3 Developer and advocate focused on building
-          decentralized applications. I work on a variety of DeFi and NFT
-          projects. You'll find some of my favorite projects below and the rest
-          of my work on Github. I am also active on Ethereum Stack Overflow and
-          Web 3 Discord communities
+        <h2 className="text-gray-600 dark:text-gray-400 font-medium mb-4 mt-4 font-light tracking-wide leading-relaxed">
+          I’m a Web 3 Developer and advocate focused on building decentralized
+          applications. I work on a variety of DeFi and NFT projects. You'll
+          find some of my favorite projects below and the rest of my work on
+          Github. I am also active on Ethereum Stack Overflow and Web 3 Discord
+          communities
         </h2>
 
         <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
